ff1d418444f45dbe95e305ce547ddcb56dac519d
felicetto13 <felice.laterza93@icloud.com>
2022-06-21 13:23:36 +0200

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+  <TheHeader></TheHeader>
+  <TheFooter></TheFooter>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TheHeader from "./components/TheHeader.vue"
+import TheFooter from "./components/TheFooter.vue"
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TheHeader, TheFooter
   }
 }
 </script>
@@ -21,8 +24,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
