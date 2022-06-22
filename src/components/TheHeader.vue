f8cbbd0feb56705484f14b262afd5087438ca1b0
felicetto13 <felice.laterza93@icloud.com>
2022-06-22 14:47:18 +0200

    modify the navbar and correct use of hover properties on links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container py-2">
-    <nav class="navbar">
+  <div class="container">
+    <nav class="my-navbar">
       <img src="../assets/img/dc-logo.png" alt="" />
       <ul class="d-flex list-unstyled">
         <li class="nav-item" v-for="(link, i) in navbarLink" :key="i">
@@ -34,7 +34,38 @@ export default {
 
 
 <style lang="scss" scoped>
+.my-navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.1rem 0;
 
+    ul {
+
+        margin: 0;
+
+        li {
+            position: relative;
+            padding-top: 1.5rem;
+            &:after {
+                transform: scaleY(0);
+                position: absolute;
+                width: 100%;
+                bottom: 0;
+                content: '';
+                height: 5px;
+                background-color: #0082f9;
+                transition:.3s;
+                transition-property: transform;
+                transform-origin: bottom;
+                transition-timing-function: ease-in-out;
+            }
+            &:hover:after{
+                transform: scaleY(1);
+            }
+        }
+        
+    }  
+}
 
 .nav-link {
     padding: 1rem 1rem;
